3e3c734446a61b4271d1e1e24c0a95b80c918efa
Rafael Matiola Arceno <96425404+rafael-ma@users.noreply.github.com>
2024-02-26 20:26:08 -0300

    Utiliza useState do React

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,41 @@
-function Form() {
+import { useState } from "react"
 
+function Form() {
     function cadastrarUsuário(event) {
         event.preventDefault()
-        console.log('Cadastrou o usuário')
+        console.log(`Usuário ${name} foi cadastrado com a senha ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Cadastro</h1>
             <form onSubmit={cadastrarUsuário}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"/>
+                    <label htmlFor="name">Nome: </label>
+                    <input 
+                        id="name" 
+                        name="name" 
+                        type="text" 
+                        placeholder="Digite seu nome"
+                        onChange={(event) => setName(event.target.value)}
+                        // Quando ocorrer uma mudança ele diparará um evento para mudar o valor guardo no state
+                    />
                 </div>
                 <div>
-        <input type="submit" value='Cadastrar' />
+                    <label htmlFor="password">Senha: </label>
+                    <input 
+                        id="password" 
+                        name="password" 
+                        type="password" 
+                        placeholder="Digite sua senha"
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value='Cadastrar' />
                 </div>
             </form>
         </div>
